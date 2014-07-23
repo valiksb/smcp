bdadbcffb9ab293c299803a9e154ace4d1fa2914
Robert Quattlebaum <darco@deepdarc.com>
2014-07-23 14:48:02 -0700

    Cleanup of various untidy bits.

--- src/plugtest/plugtest-server.c
+++ src/plugtest/plugtest-server.c
@@ -37,6 +37,8 @@
 #include <smcp/smcp.h>
 #include "plugtest-server.h"
 
+#include <smcp/string-utils.h> // For uint32_to_dec_cstr()
+
 #if CONTIKI && !defined(time)
 #define time(x)		clock_seconds()
 #endif
